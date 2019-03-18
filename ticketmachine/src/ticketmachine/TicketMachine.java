22e01ba20e67a8a3d1362de66b66acc4b63e5886
dxc33linger <48464022+dxc33linger@users.noreply.github.com>
2019-03-17 18:02:17 -0700

    Add files via upload

---
+++ ticketmachine/src/ticketmachine/TicketMachine.java
@@ -0,0 +1,75 @@
+package ticketmachine;
+
+import java.util.Scanner;
+
+public class TicketMachine {
+	// field:
+	private int price = 80;
+	private int balance;
+	private int total;
+
+	public void showPrompt() {
+		System.out.println("Welcome to buy ticket");
+		System.out.println("The price is: " + price);
+
+	}
+
+	public void insertMoney(int bill) {
+		balance += bill;
+		System.out.println("Received " + bill);
+		printBalance();
+	}
+
+	public boolean isEnough() {
+		
+		return balance >= price;
+	}
+	
+	public void printTicket() {
+		System.out.println("Tickek is coming....");
+		System.out.println("Change: " + (balance - price));
+		balance -= price;	 	
+		printBalance();
+	}		
+
+
+	public void printBalance() {
+		System.out.println("Balance: " + balance);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TicketMachine tm = new TicketMachine();
+		tm.showPrompt();
+		tm.insertMoney(100);
+		tm.isEnough();
+		System.out.println("\n");
+		
+		TicketMachine tm2 = new TicketMachine();
+		tm2.price = 60;
+		tm2.showPrompt();
+		tm2.insertMoney(70);
+		tm2.isEnough();
+		tm2.insertMoney(10);
+		tm2.isEnough();
+		
+		System.out.println("\n");
+		// auto detect bill
+		int bill;
+		TicketMachine tm3 = new TicketMachine();
+		tm3.showPrompt();
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter bill...");
+
+		do {
+			bill = in.nextInt();
+			tm3.insertMoney(bill);
+			if(tm3.isEnough()) {
+				tm3.printTicket();
+				break;
+			}
+		}while(true);
+	}
+				
+		
+}
