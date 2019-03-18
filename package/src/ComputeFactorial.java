86f193cf47026629d46cfd0232495ad02bfbb762
dxc33linger <48464022+dxc33linger@users.noreply.github.com>
2019-03-17 22:27:45 -0700

    Add files via upload

---
+++ package/src/ComputeFactorial.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ComputeFactorial {
+
+
+	public static void main(String[] args) {
+		System.out.println("ENTER binary number: ");
+		Scanner in = new Scanner(System.in);
+		String s =in.nextLine();
+		//convert decimal to binary use Integer package
+		Integer i = Integer.valueOf(s,2);
+		int ii = i;
+		System.out.println(ii);
+		
+		// Use Character package
+		System.out.println("ENTER characters: ");
+		String ss =in.nextLine();
+
+		char ch = ss.charAt(0);//return the character at index=0
+		if(Character.isLowerCase(ch))
+		{
+		
+			ch = Character.toUpperCase(ch);
+			System.out.println(ch);			
+		}
+
+		System.out.println("ENTER hello: ");
+
+		String s1 = in.next();
+		String s2 = "hello";
+		System.out.println(s1==s2);
+		System.out.println(s1.contentEquals(s2));
+
+		String s3 = "hel"+"lo";
+		System.out.println(s3==s2);
+		System.out.println(s3.contentEquals(s2));
+	}
+
+}
